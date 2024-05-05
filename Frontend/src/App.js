388a29b6a880662badac37594c74a041e1a709fa
Dhanuja Thishakya Samaranayake <d.thishakya@gmail.com>
2024-05-05 16:40:09 +0530

    update landing page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 import TicketPurchase from './components/TicketPurchase';
 import Winners from './components/Winners';
 import Navbar from './components/Navbar';
@@ -22,9 +23,8 @@ const App = () => {
         <Navbar connectWallet={connectWallet} />
           <Routes>
             <Route path="/purchase" element={<TicketPurchase purchaseNFT={handlePurchaseNFT} />} />
-            {/* Pass the contract instance as a prop */}
             <Route path="/winners" element={<Winners contract={contract} />} />
-            <Route path="/" element={<h1 className="text-3xl font-bold text-center my-8">Welcome to DeSweep - NFT Lottery Platform</h1>} />
+            <Route path="/" element={<LandingPage/>} />
           </Routes>
 
     </Router>
---
+++ Frontend/src/components/LandingPage.js
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-900 w-full">
+            <h1 className="text-4xl text-white">Welcome to DeSweep - NFT Lottery</h1>
+        </div>
+    );
+};
+
+export default LandingPage;
